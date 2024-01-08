d5dbe10ac6a6bc692b7d2af176e291e14454ae55
Jeonghwan Park <92216445+StableFluffy@users.noreply.github.com>
2024-01-08 18:03:19 +0900

    Update default.sh

--- default.sh
+++ default.sh
@@ -2,4 +2,12 @@
 
 apt-get update && apt-get install -y build-essential && pip install git+https://github.com/PygmalionAI/aphrodite-engine@dev
 
-python -m aphrodite.endpoints.openai.api_server --model $MODEL --quantization $QUANTIZATION --chat-template ../DeploIt/chat_template/$CHAT_TEMPLATE.jinja --host 0.0.0.0
+echo 'Build Done. Starting server...'
+
+CMD="python -m aphrodite.endpoints.openai.api_server --model $MODEL --chat-template ../DeploIt/chat_template/$CHAT_TEMPLATE.jinja --host 0.0.0.0"
+
+if [ -n "$QUANTIZATION" ]; then
+    CMD="$CMD --quantization $QUANTIZATION"
+fi
+
+exec $CMD
