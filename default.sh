d3c279753b7c771234ac0c4886ef0dde5bc753ef
Jeonghwan Park <92216445+StableFluffy@users.noreply.github.com>
2024-01-08 21:16:56 +0900

    Update default.sh

--- default.sh
+++ default.sh
@@ -4,7 +4,7 @@ apt-get update && apt-get install -y build-essential && pip install git+https://
 
 echo 'Build Done. Starting server...'
 
-CMD="python -m aphrodite.endpoints.openai.api_server --model $MODEL --chat-template ../DeploIt/chat_template/$CHAT_TEMPLATE.jinja --host 0.0.0.0" --dtype float16
+CMD="python -m aphrodite.endpoints.openai.api_server --model $MODEL --chat-template ../DeploIt/chat_template/$CHAT_TEMPLATE.jinja --host 0.0.0.0 --dtype float16"
 
 if [ -n "$QUANTIZATION" ]; then
     CMD="$CMD --quantization $QUANTIZATION"
