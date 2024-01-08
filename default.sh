3bfaf211dde3c20b393f03d87dc1161fe6c3dccd
Jeonghwan Park <92216445+StableFluffy@users.noreply.github.com>
2024-01-08 17:58:29 +0900

    Update default.sh

--- default.sh
+++ default.sh
@@ -2,4 +2,4 @@
 
 apt-get update && apt-get install -y build-essential && pip install git+https://github.com/PygmalionAI/aphrodite-engine@dev
 
-python -m aphrodite.endpoints.openai.api_server --model $MODEL --quantization $QUANTIZATION --chat-template ../DeploIt/chat_template/$CHAT_TEMPLATE.jinja
+python -m aphrodite.endpoints.openai.api_server --model $MODEL --quantization $QUANTIZATION --chat-template ../DeploIt/chat_template/$CHAT_TEMPLATE.jinja --host 0.0.0.0
