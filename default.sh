2ce1c74f81315ef25bba1310020bf935246a6a59
Jeonghwan Park <92216445+StableFluffy@users.noreply.github.com>
2024-01-09 19:54:49 +0900

    Update default.sh

--- default.sh
+++ default.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y build-essential && pip install git+https://github.com/PygmalionAI/aphrodite-engine
+apt-get update && apt-get install -y build-essential screen && pip install git+https://github.com/PygmalionAI/aphrodite-engine
 
 echo 'Build Done. Starting server...'
+
+pip install fastapi httpx transformers tiktoken tokenizers sentencepiece jinja2 uvicorn
 
 CMD="python -m aphrodite.endpoints.openai.api_server --model $MODEL --chat-template ../DeploIt/chat_template/$CHAT_TEMPLATE.jinja --host 0.0.0.0 --dtype float16"
 
@@ -10,4 +12,6 @@ if [ -n "$QUANTIZATION" ]; then
     CMD="$CMD --quantization $QUANTIZATION"
 fi
 
-exec $CMD
+screen -dmS server bash -c "$CMD"
+
+screen -dmS index bash -c "python index.py"
