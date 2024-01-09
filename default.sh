6b2d0d6af72d8c97c0a20afbee352f975f40afba
Jeonghwan Park <92216445+StableFluffy@users.noreply.github.com>
2024-01-09 20:05:03 +0900

    Update default.sh

--- default.sh
+++ default.sh
@@ -6,12 +6,13 @@ echo 'Build Done. Starting server...'
 
 pip install fastapi httpx transformers tiktoken tokenizers sentencepiece jinja2 uvicorn
 
-CMD="python -m aphrodite.endpoints.openai.api_server --model $MODEL --chat-template ../DeploIt/chat_template/$CHAT_TEMPLATE.jinja --host 0.0.0.0 --dtype float16"
+CMD="python -m aphrodite.endpoints.openai.api_server --model $MODEL --chat-template ./DeploIt/chat_template/$CHAT_TEMPLATE.jinja --host 0.0.0.0 --dtype float16"
 
 if [ -n "$QUANTIZATION" ]; then
     CMD="$CMD --quantization $QUANTIZATION"
 fi
 
-screen -dmS server bash -c "$CMD"
+bash -c "$CMD" &
+bash -c "python DeploIt/index.py" &
 
-screen -dmS index bash -c "python index.py"
+wait
